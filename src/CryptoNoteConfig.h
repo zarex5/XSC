d409e6d6d48408197a0a7a0117a05cc0b3c78b42
zarex5 <flytouchlucas@gmail.com>
2018-01-11 15:26:42 +0100

    Change first server ip

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "18.217.120.57:17197",
+  "18.218.50.248:17197",
   "18.217.36.165:17197"
 };
 
